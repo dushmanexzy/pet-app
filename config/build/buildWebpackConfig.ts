c7900b83486913bc1d12827b49bf53ae01afe072
Dvortsov Andrey <exzy92@gmail.com>
2023-08-15 13:41:17 +0300

    Updated with isDev

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,7 +6,7 @@ import {buildPlugins} from "./buildPlugins";
 import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
     return {
 
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         resolve: buildResolvers(),
         plugins: buildPlugins(options),
-        devtool: 'inline-source-map',
-        devServer: buildDevServer(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
