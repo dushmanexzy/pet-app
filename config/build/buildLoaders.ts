a9b2fc4f853f73d174afcc2b166fea2a3c5f5f0e
Dvortsov Andrey <exzy92@gmail.com>
2023-08-16 11:06:32 +0300

    Updated: split dev|prod css and update css modules for files with .module. in name

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -15,15 +15,18 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         test: /\.s[ac]ss$/i,
         use: [
             // Creates `style` nodes from JS strings
-            isDev
-                ? "style-loader"
-                : MiniCssExtractPlugin.loader,
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
             {
                 loader: "css-loader",
                 options: {
-                    modules: true
-                }
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
             },
             // Compiles Sass to CSS
             "sass-loader",
