de197193bcf9823e06a42dff107cbebbb74ea35e
Dvortsov Andrey <exzy92@gmail.com>
2023-09-08 10:34:17 +0300

    Updated for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {Counter} from "./components/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 import './index.scss';
 
 const App = () => {
     return (
         <div className="app">
-            Qwerty123
-            <Counter />
+            <Link to={'/'}>To Main Page</Link>
+            <Link to={'/about'}>To About Page</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />} />
+                <Route path={'/'} element={<MainPage />} />
+            </Routes>
         </div>
     );
 };
