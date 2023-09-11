1e11b21fd6a3825d317df40a48ccf43e6e238c66
Dvortsov Andrey <exzy92@gmail.com>
2023-09-11 11:29:37 +0300

    Update App toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 
-import './index.scss';
+import './styles/index.scss';
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
 
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>To Main Page</Link>
             <Link to={'/about'}>To About Page</Link>
             <Suspense fallback={<div>Loading...</div>}>
