f94dd2889c407da43562cde15c31d38e3c7a2051
Dvortsov Andrey <exzy92@gmail.com>
2023-11-01 14:28:32 +0300

    Updated webpack configs, global styles with new loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,11 +4,9 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
-    // If not ts -> babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
     }
 
     const cssLoader = {
@@ -33,8 +31,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
+    // If not ts -> babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
-        cssLoader
+        cssLoader,
     ]
 }
