d40bf17be98ddcee6c3e99369b9685b8abbc20c3
Dvortsov Andrey <exzy92@gmail.com>
2023-11-01 14:28:50 +0300

    Updated Navbar.tsx

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ThemeSwitcher";
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 
@@ -10,6 +11,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink to={'/'} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
                     To Main Page
