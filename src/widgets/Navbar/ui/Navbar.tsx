597a888b251db520b13d374888c22566bdad8d12
Dvortsov Andrey <exzy92@gmail.com>
2023-11-01 13:05:01 +0300

    Created AppLink ui

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to={'/'} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
+                    To Main Page
+                </AppLink>
+                <AppLink to={'/about'} theme={AppLinkTheme.SECONDARY}>
+                    To About Page
+                </AppLink>
+            </div>
+        </div>
+    );
+};
+
+
