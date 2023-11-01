e79ac845cdf258d3bd1b1fd3463c381e07d4a1d6
Dvortsov Andrey <exzy92@gmail.com>
2023-11-01 14:29:12 +0300

    Updated App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 import {classNames} from "shared/lib/classNames/classNames";
 
 import './styles/index.scss';
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>TOGGLE THEME</button>
         </div>
     );
 };
