76275bcb327b466007df1f0cb915f5e8d9fef73d
Dvortsov Andrey <exzy92@gmail.com>
2023-11-09 10:03:30 +0300

    Added stylelint, i18n plugin for eslint, fix all styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
     },
     extends: [
         'plugin:react/recommended',
-        'airbnb'
+        'airbnb',
+        'plugin:i18next/recommended',
     ],
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
-            'jsx': true
+            jsx: true,
         },
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
     plugins: [
         'react',
-        '@typescript-eslint'
+        '@typescript-eslint',
+        'i18next',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -34,9 +36,10 @@ module.exports = {
         'no-shadow': 'off',
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
-        'no-underscore-dangle': 'off'
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
     },
     globals: {
         __IS_DEV__: true,
-    }
-}
+    },
+};
