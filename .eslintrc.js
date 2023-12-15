de7d6b25030c0ed748fdcc66f59c0e4b96be3eb6
dushmanexzy <exzy92@gmail.com>
2023-12-15 14:34:05 +0300

    Added storybook, loki

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
         jest: true,
     },
     extends: [
-        'plugin:react/recommended',
         'airbnb',
         'plugin:i18next/recommended',
     ],
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import type webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { type BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
@@ -8,30 +8,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(
-                            resPath.includes('.module.'),
-                        ),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                        exportLocalsConvention: 'camelCase',
-                    },
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     // If not ts -> babel-loader
     const typescriptLoader = {
