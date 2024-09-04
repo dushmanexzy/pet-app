b364a4481c71b7e5c9a97b84ebf2c3f630f1c52b
Дворцов Андрей <andrey.dvortsov@lifeit.ru>
2024-09-04 16:30:43 +0200

    added .gitattributes and fixed linebrake-style for all files

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-
 import { useTranslation } from 'react-i18next';
+
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
