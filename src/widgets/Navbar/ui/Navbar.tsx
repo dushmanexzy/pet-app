f8133b3cf65417797e4a76ba1e1c1e1699087765
Дворцов Андрей <andrey.dvortsov@lifeit.ru>
2024-09-11 00:46:45 +0200

    created Modal, Portal. Updated Navbar, Button, ThemeDecorator, styles, screenshots

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { classNames } from 'shared/lib/classNames/classNames';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
+
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,17 +13,22 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState<boolean>(false);
+
+    const onToggleModal = useCallback(() => setIsAuthModal((prev) => !prev), []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
-                    {t('На главную страницу')}
-                </AppLink>
-                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                    {t('На страницу О нас')}
-                </AppLink>
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit.')}
+            </Modal>
         </div>
     );
 };
